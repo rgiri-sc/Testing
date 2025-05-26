b27f1530a7034d0963f8fa71b9ead583c324ae18
Sushant Zope <sushantzope9096@gmail.com>
2025-05-26 17:28:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const baseUrl = "https://dev.jointcommission.org";
-// const baseUrl = "http://localhost:3000";
+// const baseUrl = 'http://localhost:3000';
 const domestic = "en-us";
 const international = "en";
 const local = domestic;
 
 function clickHandler(event) {
-  // alert("Clicked!");
   let anchor = event.target.closest("a");
 
-  if (anchor && anchor.href) {
+  if (anchor?.href) {
     if (anchor.getAttribute("target") === "_blank") {
       return;
     }
@@ -78,7 +77,6 @@ function resolveUrlsAndImages(element) {
         }
       }
     });
-    //console.log("MutationObserver callback executed");
   });
   menuObserver.observe(element, {
     childList: true,
@@ -88,62 +86,13 @@ function resolveUrlsAndImages(element) {
   });
 }
 
-function interceptApiCalls() {
-  // Store original fetch
-  const originalFetch = window.fetch;
-
-  // Override fetch to redirect API calls
-  window.fetch = function (url, options) {
-    console.log("Fetch called with URL:", url);
-
-    let modifiedUrl = url;
-
-    // Check if this is a relative URL or one from the current origin
-    if (typeof url === "string") {
-      const currentOrigin = window.location.origin;
-
-      // If it's a relative URL or from current origin but should be from baseUrl
-      if (
-        url.includes("api/suggest-text") ||
-        url.includes("api/auth/session") ||
-        url.includes("_next/data")
-      ) {
-        console.log("Intercepting API call:", url);
-
-        // Extract the path part
-        let path = url;
-        path = url.replace("_next/data", "api/_next/data");
-        if (url.startsWith(currentOrigin)) {
-          path = url.substring(currentOrigin.length);
-        }
-
-        // Create new URL with baseUrl
-        modifiedUrl = `${baseUrl}${path}`;
-        //console.log(`Redirecting fetch from ${url} to ${modifiedUrl}`);
-      }
-    }
-
-    // Call original fetch with modified URL
-    return originalFetch.call(this, modifiedUrl, options);
-  };
-
-  //console.log("API call interception enabled");
-}
-
 // Function to disable Next.js hydration but allow search parameters
 function disableNextJsHydration() {
-  //console.log("Disabling Next.js hydration with search parameter support");
-
   // Completely disable the Next.js router except for URL parameter updates
-  if (window.next && window.next.router) {
+  if (window?.next?.router) {
     // Store original methods
     const originalPush = window.next.router.push;
-    const originalReplace = window.next.router.replace;
-    // Override push to only allow search parameter updates
-
     window.next.router.push = function (url, as, options) {
-      //console.log("next.router.push called with:", url, as, options);
-
       // If URL dosen't start with /, add it
       if (!url.startsWith("/")) {
         return;
@@ -151,12 +100,9 @@ function disableNextJsHydration() {
 
       // only block navigation if it's a pathname is "/header"
       if (url.pathname === "/header") {
-        //console.log("Blocking navigation to:", url);
         url.pathname = url.pathname.replace("/header", "/");
         return originalPush.call(this, url, as, options);
-        // return Promise.resolve(false);
       } else {
-        //console.log("Allowing navigation to:", url);
         if (
           url.startsWith(`/${domestic}`) ||
           url.startsWith(`/${international}`)
@@ -171,25 +117,13 @@ function disableNextJsHydration() {
 
     // Similar override for replace
     window.next.router.replace = function (url, as, options) {
-      /* console.log("replace: Blocking navigation to: url: ", url);
-      console.log("replace: Blocking navigation to: as: ", as);
-      console.log("replace: Blocking navigation to: url: ", options); */
-
       if (url === "/header?") {
-        console.log("Blocking navigation to:", url);
-        url = url.replace("/header?", "");
-        // return originalReplace.call(this, url, as, options);
+        url.replace("/header?", "");
         return Promise.resolve(false);
       }
 
-      /* if (url?.query?.includes("rfkid_") || url?.query?.includes("rfkid_")) {
-        // return original method
-        return originalReplace(url, as, options);
-      } */
       // If it's just a search parameter update (same pathname)
       if (typeof url === "object" && url.query) {
-        //console.log("Allowing search parameter update:", url.query);
-
         // Create URL with search parameters
         const searchParams = new URLSearchParams();
         for (const key in url.query) {
@@ -207,41 +141,68 @@ function disableNextJsHydration() {
 
       return Promise.resolve(false);
     };
-
-    //console.log("Next.js router methods modified to allow search parameters");
   }
 }
 
+function networkListener() {
+  const originalFetch = window.fetch;
+  const redirectUrls = [
+    "/api/auth/session",
+    "/api/auth/_log",
+    "/en-us/geolocation",
+    "/en/geolocation",
+    "/api/suggest-text",
+  ];
+  window.fetch = function (url, options) {
+    if (
+      redirectUrls.find((_url) => url.startsWith(_url)) &&
+      typeof url === "string"
+    ) {
+      const newUrl = baseUrl + url;
+      return originalFetch.call(this, newUrl, options);
+    }
+
+    if (url.startsWith("/_next") && typeof url === "string") {
+      const newUrl = baseUrl?.endsWith("/")
+        ? baseUrl
+        : `${baseUrl}/` + "api/philosopher?file=" + baseUrl + url;
+      return originalFetch.call(this, newUrl, options);
+    }
+    return originalFetch.call(this, url, options);
+  };
+}
+
+function updateUrls(htmlString) {
+  htmlString = htmlString.replace(/href="\/api/g, 'href="' + baseUrl + "/api");
+  htmlString = htmlString.replace(
+    /src="\/_next/g,
+    'src="' + baseUrl + "/_next"
+  );
+  htmlString = htmlString.replace(/srcset="[^"]*\/_next/g, (match) =>
+    match.replace("/_next", baseUrl + "/_next")
+  );
+  htmlString = htmlString.replace(
+    /href="\/en-us/g,
+    'href="' + baseUrl + "/en-us"
+  );
+  htmlString = htmlString.replace(/href="\/en/g, 'href="' + baseUrl + "/en");
+  htmlString = htmlString.replaceAll(
+    baseUrl + "/_next",
+    baseUrl + "/api/philosopher?file=" + baseUrl + "/_next"
+  );
+  return htmlString;
+}
+
 document.addEventListener("DOMContentLoaded", async function () {
-  interceptApiCalls();
+  networkListener();
   const headerElement = document.getElementById("external-header");
   const footerContainer = document.getElementById("external-footer");
 
-  if (headerElement) {
-    //console.log("Setting up MutationObserver for header element");
-    resolveUrlsAndImages(headerElement);
-    //console.log("MutationObserver started for header element");
-  } else {
-    console.warn("Header element not found for observation");
-  }
-
-  if (footerContainer) {
-    //console.log("Setting up MutationObserver for footer container");
-    resolveUrlsAndImages(footerContainer);
-    //console.log("MutationObserver started for footer container");
-  } else {
-    console.warn("Footer container not found for observation");
-  }
-
   try {
-    //console.log("DOM fully loaded");
-    //console.log("Fetching header from:", `${baseUrl}/${local}/header`);
     const headerResponse = await fetch(`${baseUrl}/${local}/header`);
     if (!headerResponse.ok)
       throw new Error(`Failed to load header: ${headerResponse.status}`);
     const headerHTML = await headerResponse.text();
-    //console.log("Raw header HTML received, length:", headerHTML.length);
-    //console.log("Header HTML preview:", headerHTML.substring(0, 200) + "...");
     const headerContainer = document.getElementById("external-header");
     if (!headerContainer) {
       console.error(
@@ -249,17 +210,19 @@ document.addEventListener("DOMContentLoaded", async function () {
       );
       return;
     }
-    headerContainer.addEventListener("click", clickHandler);
-    headerContainer.innerHTML = headerHTML;
-    //console.log("Header HTML inserted into DOM");
+    const shadowHeader = headerElement.attachShadow({ mode: "open" });
+
+    const template = document.createElement("template");
+    template.innerHTML = updateUrls(headerHTML);
+    shadowHeader.appendChild(template.content);
+
+    shadowHeader.addEventListener("click", clickHandler);
 
     const footerUrl = `${baseUrl}/${local}/footer`;
-    //console.log("Fetching footer from:", footerUrl);
     const footerResponse = await fetch(footerUrl);
     if (!footerResponse.ok)
       throw new Error(`Failed to load footer: ${footerResponse.status}`);
     const footerHTML = await footerResponse.text();
-    //console.log("Raw footer HTML received, length:", footerHTML.length);
 
     if (!footerContainer) {
       console.error(
@@ -267,12 +230,97 @@ document.addEventListener("DOMContentLoaded", async function () {
       );
       return;
     }
-    //console.log("Footer container found");
-    footerContainer.innerHTML = footerHTML;
-    //console.log("Footer HTML inserted into DOM");
 
-    executeMatchingScripts(headerContainer);
-    //console.log("Header scripts executed");
+    const shadowFooter = footerContainer.attachShadow({ mode: "open" });
+    const templateFooter = document.createElement("template");
+    templateFooter.innerHTML = updateUrls(footerHTML);
+    shadowFooter.appendChild(templateFooter.content);
+
+    const nextData = shadowHeader.getElementById("__NEXT_DATA__");
+    document.body.appendChild(nextData);
+
+    const originalGetElementById = document.getElementById;
+    let headerResolved = false;
+    let footerResolved = false;
+
+    document.getElementById = function (id, ...args) {
+      if (id === "__next") {
+        const headerContainer = shadowHeader.getElementById(id, ...args);
+        if (!headerResolved && headerContainer) {
+          headerResolved = true;
+          resolveUrlsAndImages(headerContainer);
+          return headerContainer;
+        }
+        const footerContainer = shadowFooter.getElementById(id, ...args);
+        if (!footerResolved && footerContainer) {
+          footerResolved = true;
+          resolveUrlsAndImages(footerContainer);
+          return footerContainer;
+        }
+      }
+
+      return originalGetElementById.call(document, id, ...args);
+    };
+
+    const originalQuerySelector = document.querySelector;
+    document.querySelector = function (selector, ...args) {
+      if (selector === "#__next_css__DO_NOT_USE__") {
+        return shadowHeader.childNodes[0];
+      }
+      return originalQuerySelector.call(document, selector, ...args);
+    };
+
+    executeMatchingScripts(shadowHeader);
+    resolveUrlsAndImages(shadowHeader);
+    resolveUrlsAndImages(shadowFooter);
+
+    setInterval(() => {
+      const imgElements = shadowHeader.querySelectorAll("img");
+      imgElements.forEach((img) => {
+        processImageSrc(img, baseUrl);
+      });
+
+      const footerImgElements = shadowFooter.querySelectorAll("img");
+      footerImgElements.forEach((img) => {
+        processImageSrc(img, baseUrl);
+      });
+    }, 1000);
+
+    setTimeout(() => {
+      const linkElements = shadowHeader.querySelectorAll(
+        'link[rel="stylesheet"]'
+      );
+      linkElements.forEach(async (link) => {
+        try {
+          const cssUrl = link.getAttribute("href");
+          if (cssUrl) {
+            const response = await fetch(cssUrl);
+            const cssText = await response.text();
+
+            let startIndex = 0;
+            while (
+              (startIndex = cssText.indexOf("@font-face{", startIndex)) !== -1
+            ) {
+              let endIndex = cssText.indexOf("}", startIndex);
+              if (endIndex === -1) break;
+
+              // Include the closing brace
+              endIndex++;
+
+              const fontFaceRule = cssText.substring(startIndex, endIndex);
+
+              const styleElement = document.createElement("style");
+              styleElement.textContent = fontFaceRule;
+              document.head.appendChild(styleElement);
+
+              startIndex = endIndex;
+            }
+          }
+        } catch (error) {
+          console.error("Error processing CSS file:", error);
+        }
+      });
+    }, 1000);
   } catch (error) {
     console.error("Error loading external components:", error);
   }
@@ -301,28 +349,71 @@ function processAnchorHref(anchorElement, baseUrl) {
     }
   }
 }
+
 function processImageSrc(imgElement, baseUrl) {
-  if (imgElement.hasAttribute("src")) {
-    const src = imgElement.getAttribute("src");
-    if (
-      src &&
-      !src.startsWith("http") &&
-      !src.startsWith("//") &&
-      !src.startsWith("data:") &&
-      !src.startsWith("javascript:")
-    ) {
-      let newSrc;
-      if (src.startsWith("/")) {
-        newSrc = `${baseUrl}${src}`;
-      } else {
-        newSrc = `${baseUrl}/${src}`;
-      }
-      imgElement.src = newSrc;
-    }
-  }
   if (imgElement.hasAttribute("srcset")) {
     const srcset = imgElement.getAttribute("srcset");
-    imgElement.setAttribute("srcset", processSrcsetValue(srcset, baseUrl));
+    if (!srcset.startsWith(baseUrl + "/api/philosopher?file=")) {
+      const _srcset = srcset
+        .split(",")
+        .map((part) => {
+          const [url, descriptor] = part.trim().split(/\s+/);
+          let modifiedUrl;
+
+          // Handle /_next/image URLs in srcset
+          if (url.startsWith("/_next/image")) {
+            modifiedUrl = `${baseUrl}/api/philosopher?file=${baseUrl}${url}`;
+          } else if (url.startsWith(baseUrl)) {
+            modifiedUrl = url.replace(
+              baseUrl,
+              baseUrl + "/api/philosopher?file=" + baseUrl
+            );
+          } else if (url.startsWith("/_next")) {
+            modifiedUrl = `${baseUrl}/api/philosopher?file=${baseUrl}${url}`;
+          } else if (!url.startsWith("http") && !url.startsWith("//")) {
+            // Handle relative URLs
+            modifiedUrl = `${baseUrl}/api/philosopher?file=${baseUrl}${
+              url.startsWith("/") ? url : "/" + url
+            }`;
+          } else {
+            modifiedUrl = url;
+          }
+
+          return descriptor ? `${modifiedUrl} ${descriptor}` : modifiedUrl;
+        })
+        .join(",");
+
+      imgElement.setAttribute("srcset", _srcset);
+    }
+  }
+
+  if (imgElement.hasAttribute("src")) {
+    const src = imgElement.getAttribute("src");
+    if (!src.startsWith(baseUrl + "/api/philosopher?file=")) {
+      // Special handling for /_next/image URLs
+      if (src.startsWith("/_next/image")) {
+        const newSrc = `${baseUrl}/api/philosopher?file=${baseUrl}${src}`;
+        imgElement.src = newSrc;
+        return;
+      }
+
+      const _src = src.startsWith(baseUrl)
+        ? src.replace(baseUrl, baseUrl + "/api/philosopher?file=" + baseUrl)
+        : src.startsWith("/_next")
+        ? `${baseUrl}/api/philosopher?file=${baseUrl}${src}`
+        : src;
+
+      if (
+        src &&
+        !src.startsWith("http") &&
+        !src.startsWith("//") &&
+        !src.startsWith("data:") &&
+        !src.startsWith("javascript:")
+      ) {
+        imgElement.src = _src;
+        return;
+      }
+    }
   }
 }
 
@@ -356,109 +447,22 @@ function processSrcsetValue(srcsetContent, baseUrl) {
 
 function executeMatchingScripts(container) {
   const scripts = container.querySelectorAll("script");
-  //console.log(`Found ${scripts.length} scripts in container`);
   scripts.forEach((oldScript, index) => {
     if (
-      (oldScript.src && oldScript.src.includes("_next")) ||
-      oldScript.id === "__NEXT_DATA__"
+      oldScript?.src?.includes("_next") ||
+      oldScript?.id === "__NEXT_DATA__"
     ) {
-      /*console.log(
-        `Executing script ${index + 1}:`,
-        oldScript.src || oldScript.id || "inline script"
-      );*/
       const newScript = document.createElement("script");
       Array.from(oldScript.attributes).forEach((attr) => {
         newScript.setAttribute(attr.name, attr.value);
       });
       newScript.onload = () => {
-        //console.log(`Executed script ${index + 1}:`, oldScript.src || "inline");
         disableNextJsHydration();
       };
       newScript.textContent = oldScript.textContent;
       oldScript.parentNode.replaceChild(newScript, oldScript);
     } else {
-      //console.log(`Removing non-matching script ${index + 1}`);
       oldScript.parentNode.removeChild(oldScript);
     }
   });
-}
-
-function processRelativeUrls(html, baseUrl) {
-  //console.log("Processing relative URLs in HTML content...");
-  try {
-    html = html.replace(
-      /(href)=(["'])(\/[^"']+|[^"':][^"']+)(["'])/gi,
-      function (match, attr, quote, url, endQuote) {
-        if (!url) return match;
-        url = url.replace(/&amp;/g, "&");
-        if (url.startsWith(`${baseUrl}/_next/static/media`)) {
-          return `${attr}=${quote}${url?.replace(
-            "_next/static/media",
-            "api/_next/static/media"
-          )}${endQuote}`;
-        }
-      }
-    );
-    //console.log("URL processing complete");
-    return html;
-  } catch (error) {
-    console.error("Error in processRelativeUrls:", error);
-  }
-}
-function domBasedProcessRelativeUrls(html, baseUrl) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, "text/html");
-  doc.querySelectorAll("img").forEach((img) => {
-    if (
-      img.src &&
-      !img.src.startsWith("http") &&
-      !img.src.startsWith("//") &&
-      !img.src.startsWith("data:")
-    ) {
-      img.src = img.src.startsWith("/")
-        ? `${baseUrl}${img.src}`
-        : `${baseUrl}/${img.src}`;
-    }
-    if (img.srcset) {
-      img.srcset = processSrcsetValue(img.srcset, baseUrl);
-    }
-  });
-  doc.querySelectorAll("a").forEach((a) => {
-    if (
-      a.href &&
-      !a.href.startsWith("http") &&
-      !a.href.startsWith("//") &&
-      !a.href.startsWith("mailto:") &&
-      !a.href.startsWith("#") &&
-      !a.href.startsWith("javascript:") &&
-      !a.href.startsWith("tel:")
-    ) {
-      a.href = a.href.startsWith("/")
-        ? `${baseUrl}${a.href}`
-        : `${baseUrl}/${a.href}`;
-    }
-  });
-  doc.querySelectorAll('link[rel="stylesheet"]').forEach((link) => {
-    if (
-      link.href &&
-      !link.href.startsWith("http") &&
-      !link.href.startsWith("//")
-    ) {
-      link.href = link.href.startsWith("/")
-        ? `${baseUrl}${link.href}`
-        : `${baseUrl}/${link.href}`;
-    }
-  });
-  doc.querySelectorAll("script[src]").forEach((script) => {
-    if (
-      script.src &&
-      !script.src.startsWith("http") &&
-      !script.src.startsWith("//")
-    ) {
-      script.src = script.src.startsWith("/")
-        ? `${baseUrl}${script.src}`
-        : `${baseUrl}/${script.src}`;
-    }
-  });
-  return doc.documentElement.outerHTML;
 }
