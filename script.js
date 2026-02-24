b0724b704d65774600cd0f9426b35f904593be70
Sushant Zope <sushantzope9096@gmail.com>
2026-02-24 12:22:24 +0530

    Update baseUrl to development environment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://www.jointcommission.org";
+const baseUrl = "https://dev.jointcommission.org";
 // const baseUrl = "http://localhost:3000";
 const domestic = "en-us";
 const international = "en";
@@ -494,4 +494,5 @@ function executeMatchingScripts(container) {
     }
   });
 }
+
 
